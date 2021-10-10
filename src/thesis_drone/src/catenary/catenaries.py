a363d9e088a319b032f4e5683f9857f0d1bbadfa
marios-stam <mariosstamatopoulos1@gmail.com>
2021-10-11 02:41:21 +0300

    3D catenaries needs extra work

---
+++ src/thesis_drone/src/catenary/catenaries.py
@@ -0,0 +1,87 @@
+from math_utils import Transformation, sqrt, sinh, cosh, tanhi
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+DEBUG = 1
+
+
+def getCatenaryCurve2D(P1, P2, L):
+    if DEBUG:
+        print("Getting Catenary2D...")
+        print("P1:", P1)
+        print("P2:", P2)
+        print("L :", L)
+
+    x1, y1 = P1
+    x2, y2 = P2
+
+    dx = x2-x1
+    dy = y2-y1
+
+    xb = (x1+x2)/2
+    yb = (x1+x2)/2
+
+    r = sqrt(L**2-dy**2)/dx
+
+    # rA=sinh(A)
+
+    A = 0.01
+    dA = 0.0001
+    left = r*A
+    right = sinh(A)
+    # print(left,right)
+
+    while left >= right:
+        left = r*A
+        right = sinh(A)
+        A = A+dA
+
+    A = A-dA
+
+    a = dx/(2*A)
+    b = xb-a*tanhi(dy/L)
+    c = y1-a*cosh((x1-b)/a)
+
+    xs, ys = [], []
+    x = x1
+    ddx = 0.001
+    while x < x2:
+        y = a*cosh((x-b)/a)+c
+        xs.append(x)
+        ys.append(y)
+        x = x+ddx
+    return xs, ys
+
+
+def getCatenaryCurve3D(P1, P2, L):
+    x1, y1, z1 = P1
+    x2, y2, z2 = P2
+
+    dx = x2-x1
+    dy = y2-y1
+
+    angle = math.atan2(dy, dx)
+
+    print("angle in degrees:", math.degrees(angle))
+
+    rotation = [0, 0, math.degrees(angle)]
+    trans = Transformation(rotation, translation=P1)
+
+    p2_1 = trans.transformPoint(P2)
+
+    start = [0, 0, 0]
+    xs2D, ys2D = getCatenaryCurve2D(start[:2], p2_1[:2], L)
+    print(xs2D)
+    print(ys2D)
+
+    plt.plot(xs2D, ys2D)
+
+
+if __name__ == "__main__":
+    # xs, ys = getCatenaryCurve2D(P1, P2, L)
+    P1 = np.array([1, 1, 0])
+    P2 = np.array([2, 2, 0])
+    L = 2
+    getCatenaryCurve3D(P1, P2, L)
+    plt.show()
---
+++ src/thesis_drone/src/catenary/math_utils.py
@@ -0,0 +1,87 @@
+from numpy.core.defchararray import translate
+from geometry_msgs.msg import Vector3
+from math import degrees, exp, log, sqrt, atan2
+import math
+import tf.transformations as transformations
+import numpy as np
+
+
+def cosh(z):
+    return((exp(z)+exp(-z))/2)
+
+
+def sinh(z):
+    return((exp(z)-exp(-z))/2)
+
+
+def tanhi(z):
+    return((1/2)*log((1+z)/(1-z)))
+
+
+def getTransformationMatrix(rotation, translation):
+    """
+    Returns the transformation matrix frame specified by rotation and translation .
+
+    Parameters
+    ----------
+    rotation : array,Vector3
+        Rotation angles on x,y,z axes.
+    translation : array,Vector3
+        Translation of the new frame.
+    Returns
+    -------
+    output : ndarray
+        Transformation Matrix.
+    """
+    origin, xaxis, yaxis, zaxis = (0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1)
+    I = transformations.identity_matrix()
+    Rx = transformations.rotation_matrix(math.radians(rotation[0]), xaxis)
+    Ry = transformations.rotation_matrix(math.radians(rotation[1]), yaxis)
+    Rz = transformations.rotation_matrix(math.radians(rotation[2]), zaxis)
+    R = transformations.concatenate_matrices(Rx, Ry, Rz)
+
+    T = transformations.translation_matrix(-translation)
+    M = transformations.concatenate_matrices(R, T)
+
+    return M
+
+
+def trig(theta):
+    return math.cos(theta), math.sin(theta), math.tan(theta)
+
+
+class Transformation():
+    def __init__(self, rotation, translation) -> None:
+        self.matrix = getTransformationMatrix(rotation, translation)
+        self.inv_matrix = np.linalg.inv(self.matrix)
+
+        print(np.dot(self.inv_matrix, self.matrix))
+
+    def transformPoint(self, p):
+        if len(p) == 3:
+            p = np.append(p, 1)
+
+        p_m = np.dot(self.matrix, p)
+        return p_m
+
+    def inverseTransformPoint(self, p):
+        if len(p) == 3:
+            p = np.append(p, 1)
+
+        p_m = np.dot(self.inv_matrix, p)
+        return p_m
+
+
+if __name__ == "__main__":
+    P2 = np.array([2, 2, 0])
+    translation = np.array([1, 1, 0])
+    rotation = np.array([0, 0, 45])
+
+    L = 0.65
+
+    trans = Transformation(rotation, translation)
+    p2_1 = trans.transformPoint(P2)
+    p1_2 = trans.inverseTransformPoint(p2_1)
+
+    print(p2_1)
+    print(p1_2)
